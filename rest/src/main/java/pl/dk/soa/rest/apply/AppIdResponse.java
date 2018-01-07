f99dc341bd0b0e9a9c0321a6ef24510e8e1ef4dd
dkublik <dkublik@gmail.com>
2018-01-07 17:06:04 +0100

    other methods for apply

---
+++ rest/src/main/java/pl/dk/soa/rest/apply/AppIdResponse.java
@@ -0,0 +1,14 @@
+package pl.dk.soa.rest.apply;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AppIdResponse {
+    private String applicationId;
+}
--- rest/src/main/java/pl/dk/soa/rest/apply/ApplyController.java
+++ rest/src/main/java/pl/dk/soa/rest/apply/ApplyController.java
@@ -5,7 +5,11 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static org.springframework.http.HttpStatus.ACCEPTED;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.UUID.randomUUID;
+import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 @RestController
@@ -13,10 +17,38 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Api(description = "job applications")
 class ApplyController {
 
+    private Map<String, Application> applications = new HashMap<>();
+
     @PostMapping()
     @ApiOperation(value = "apply for job")
-    ResponseEntity<Void> applyForJob(@RequestBody Application application) {
-        return new ResponseEntity<>(ACCEPTED);
+    ResponseEntity<AppIdResponse> applyForJob(@RequestBody Application application) {
+        String applicationId = randomUUID().toString();
+        applications.put(applicationId, application);
+        return new ResponseEntity<>(new AppIdResponse(applicationId), ACCEPTED);
+    }
+
+    @PutMapping("/{applicationId}")
+    @ApiOperation(value = "apply for job with app ID")
+    ResponseEntity<AppIdResponse> applyForJob(@PathVariable String applicationId, @RequestBody Application application) {
+        applications.put(applicationId, application);
+        return new ResponseEntity<>(new AppIdResponse(applicationId), ACCEPTED);
+    }
+
+    @GetMapping("/{applicationId}")
+    @ApiOperation(value = "show Application")
+    ResponseEntity<Application> showApplication(@PathVariable String applicationId) {
+        Application application = applications.get(applicationId);
+        if (application == null) {
+            return new ResponseEntity<>(NOT_FOUND);
+        }
+        return new ResponseEntity<>(application, OK);
+    }
+
+    @DeleteMapping("/{applicationId}")
+    @ApiOperation(value = "show Application")
+    ResponseEntity<Void> deleteApplication(@PathVariable String applicationId) {
+        applications.remove(applicationId);
+        return new ResponseEntity<>(NO_CONTENT);
     }
 
 }
